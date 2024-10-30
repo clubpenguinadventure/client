8a656e4f4459f326313c7bac51f5b3d668f59bc3
sky <67554336+sky-is-winning@users.noreply.github.com>
2024-10-30 21:55:15 +0000

    add new village

--- src/scenes/rooms/village/Village.js
+++ src/scenes/rooms/village/Village.js
@@ -1,7 +1,6 @@
 /* START OF COMPILED CODE */
 
 import RoomScene from "../RoomScene";
-import SimpleButton from "../../components/SimpleButton";
 import Button from "../../components/Button";
 import MoveTo from "../../components/MoveTo";
 /* START-USER-IMPORTS */
@@ -13,14 +12,14 @@ export default class Village extends RoomScene {
         super("Village");
 
         /** @type {Phaser.GameObjects.Sprite} */
-        this.rings;
-        /** @type {Phaser.GameObjects.Sprite} */
-        this.chair;
+        this.chairs_back;
         /** @type {Phaser.GameObjects.Image} */
         this.lift;
+        /** @type {Phaser.GameObjects.Sprite} */
+        this.chairs_front;
         /** @type {Phaser.GameObjects.Image} */
         this.toursText;
-        /** @type {Array<Phaser.GameObjects.Container|Phaser.GameObjects.Image|Phaser.GameObjects.Sprite>} */
+        /** @type {Array<Phaser.GameObjects.Container|Phaser.GameObjects.Image>} */
         this.sort;
 
 
@@ -46,98 +45,86 @@ export default class Village extends RoomScene {
     /** @returns {void} */
     _create() {
 
-        // rings
-        const rings = this.add.sprite(1377, 584, "village", "rings/rings0001");
-        rings.setInteractive(new Phaser.Geom.Polygon("1455 456 1455 643 1295 643"), Phaser.Geom.Polygon.Contains);
-        rings.setOrigin(0.9297771775827144, 0.906832298136646);
-
         // bg
-        const bg = this.add.image(-26, -12, "village", "bg");
+        const bg = this.add.image(0, 0, "village", "bg");
         bg.setOrigin(0, 0);
 
-        // lodgeFront
-        const lodgeFront = this.add.image(1086, 384, "village", "lodge_front");
-        lodgeFront.setOrigin(0.4789272030651341, 0.5352112676056338);
+        // snowmound
+        const snowmound = this.add.image(198.7883349873273, 540.3093132550932, "village", "snowmound");
+        snowmound.setOrigin(0.13078179933376796, 0.5628222013073888);
 
-        // lodgeSnow
-        const lodgeSnow = this.add.image(1067, 423, "village", "lodge_snow");
-        lodgeSnow.setOrigin(0.46332046332046334, 0.47435897435897434);
+        // lodge
+        const lodge = this.add.container(760, -36);
 
-        // lodgeStairs
-        const lodgeStairs = this.add.image(931, 459, "village", "lodge_stairs");
-        lodgeStairs.setOrigin(0, 0.7868852459016393);
+        // lodgeBuilding
+        const lodgeBuilding = this.add.image(0, 516, "village", "lodge");
+        lodge.add(lodgeBuilding);
 
         // lodgeDoor
-        const lodgeDoor = this.add.image(1024, 186, "village", "lodge_door");
-        lodgeDoor.setOrigin(0, 0);
+        const lodgeDoor = this.add.image(357, 338, "village", "lodge-door");
+        lodge.add(lodgeDoor);
+
+        // lodgeFront
+        const lodgeFront = this.add.image(315.93504341693347, 425.62441646884656, "village", "lodge-front");
+        lodgeFront.setOrigin(0.494671336828891, 0.2749874718122159);
+        lodge.add(lodgeFront);
 
         // smoke
-        const smoke = this.add.sprite(1093, -39, "village", "smoke0001");
-        smoke.setOrigin(0, 0);
-        smoke.play("village/smoke");
+        const smoke = this.add.sprite(348, 72, "village", "lodge-steam0001");
+        smoke.play("village-lodge-steam");
+        lodge.add(smoke);
 
-        // sportShop
-        const sportShop = this.add.container(1185, 428.3932671887646);
+        // epf
+        const epf = this.add.container(0, 0);
 
-        // sport
-        const sport = this.add.image(0, -395.3932695094585, "village", "sport");
-        sport.setOrigin(0, 0);
-        sportShop.add(sport);
+        // facility
+        const facility = this.add.image(0, 0, "village", "epf");
+        facility.setOrigin(0, 0);
+        epf.add(facility);
 
-        // sportDoor
-        const sportDoor = this.add.image(126, -208.3932695094585, "village", "sport_door");
-        sportDoor.setOrigin(0, 0);
-        sportShop.add(sportDoor);
-
-        // ring
-        const ring = this.add.image(1468, 566, "village", "ring");
-        ring.setOrigin(0.5, 0.8181818181818182);
-
-        // sled
-        const sled = this.add.image(1415, 576, "village", "sled");
-        sled.setOrigin(0.49206349206349204, 2);
+        // epfDoor
+        const epfDoor = this.add.image(1408, 351, "village", "epf-door");
+        epf.add(epfDoor);
 
         // chairLift
         const chairLift = this.add.layer();
 
-        // chair
-        const chair = this.add.sprite(-116, -136, "village", "chair/chair0001");
-        chair.setOrigin(0, 0);
-        chair.play("village/chair");
-        chairLift.add(chair);
+        // chairs_back
+        const chairs_back = this.add.sprite(363, 128, "village", "chairs/chairs-back0032");
+        chairs_back.visible = false;
+        chairLift.add(chairs_back);
 
         // lift
-        const lift = this.add.image(85, -5, "village", "lift");
+        const lift = this.add.image(0, 0, "village", "skilift");
         lift.setOrigin(0, 0);
         chairLift.add(lift);
 
+        // chairs_front
+        const chairs_front = this.add.sprite(294, 181, "village", "chairs/chairs-front0149");
+        chairs_front.visible = false;
+        chairLift.add(chairs_front);
+
         // rightSign
-        const rightSign = this.add.image(1465, 667, "village", "right_sign");
-        rightSign.setOrigin(0.4794520547945205, 0.7831325301204819);
+        const rightSign = this.add.image(760, 480, "village", "arrow-rightside");
 
         // leftSign
-        const leftSign = this.add.image(139, 675, "village", "left_sign");
-        leftSign.setOrigin(0.4444444444444444, 0.5217391304347826);
+        const leftSign = this.add.image(760, 480, "village", "arrow-leftside");
 
         // tourBooth
-        const tourBooth = this.add.container(797, 476);
+        const tourBooth = this.add.container(810, 420);
 
         // tours
-        const tours = this.add.image(0, 0, "village", "tours");
+        const tours = this.add.image(1, 62, "village", "tours-btn");
         tours.setOrigin(0.5028901734104047, 0.8894230769230769);
         tourBooth.add(tours);
 
         // toursText
-        const toursText = this.add.image(3, -144, "village", "tours_text");
+        const toursText = this.add.image(3, -144, "village", "tours-text");
         toursText.setOrigin(0.5034965034965035, 0.5);
         tourBooth.add(toursText);
 
         // lists
-        const sort = [tourBooth, leftSign, rightSign, sled, rings, ring, sportShop, lodgeStairs, lodgeSnow, lodgeFront];
-
-        // rings (components)
-        const ringsSimpleButton = new SimpleButton(rings);
-        ringsSimpleButton.hoverCallback = () => this.onRingsOver();
+        const sort = [tourBooth, leftSign, rightSign, epf, lodgeFront, snowmound];
 
         // lodgeDoor (components)
         const lodgeDoorButton = new Button(lodgeDoor);
@@ -146,12 +133,12 @@ export default class Village extends RoomScene {
         lodgeDoorMoveTo.x = 1000;
         lodgeDoorMoveTo.y = 400;
 
-        // sportDoor (components)
-        const sportDoorButton = new Button(sportDoor);
-        sportDoorButton.activeFrame = false;
-        const sportDoorMoveTo = new MoveTo(sportDoor);
-        sportDoorMoveTo.x = 1340;
-        sportDoorMoveTo.y = 480;
+        // epfDoor (components)
+        const epfDoorButton = new Button(epfDoor);
+        epfDoorButton.activeFrame = false;
+        const epfDoorMoveTo = new MoveTo(epfDoor);
+        epfDoorMoveTo.x = 1340;
+        epfDoorMoveTo.y = 480;
 
         // tours (components)
         const toursButton = new Button(tours);
@@ -161,9 +148,9 @@ export default class Village extends RoomScene {
         toursButton.activeFrame = false;
         toursButton.pixelPerfect = true;
 
-        this.rings = rings;
-        this.chair = chair;
+        this.chairs_back = chairs_back;
         this.lift = lift;
+        this.chairs_front = chairs_front;
         this.toursText = toursText;
         this.sort = sort;
 
@@ -175,12 +162,13 @@ export default class Village extends RoomScene {
 
     create() {
         super.create()
-
-        this.chair.on('animationupdate', this.onChairUpdate, this)
-    }
-
-    onChairUpdate(animation, frame) {
-        this.chair.depth = frame.index > 90 ? 1 : -1
+        this.chairs_back.play('village-chairs-back')
+        this.chairs_back.on('animationcomplete', () => {
+            this.chairs_front.play('village-chairs-front')
+        })
+        this.chairs_front.on('animationcomplete', () => {
+            this.chairs_back.play('village-chairs-back')
+        })
     }
 
     onRingsOver() {
@@ -188,12 +176,12 @@ export default class Village extends RoomScene {
     }
 
     onToursOver() {
-        this.toursText.setFrame('tours_text_large')
+        //this.toursText.setFrame('tours_text_large')
         this.toursText.y -= 6
     }
 
     onToursOut() {
-        this.toursText.setFrame('tours_text')
+        //this.toursText.setFrame('tours_text')
         this.toursText.y += 6
     }
 
