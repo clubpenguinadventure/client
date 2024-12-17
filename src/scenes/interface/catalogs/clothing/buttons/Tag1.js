e549b3af14449d327293c754bbe881560d626113
sky <67554336+sky-is-winning@users.noreply.github.com>
2024-12-17 15:37:16 +0000

    catalog mostly finished

--- src/scenes/interface/catalogs/clothing/buttons/Tag1.js
+++ src/scenes/interface/catalogs/clothing/buttons/Tag1.js
@@ -8,7 +8,7 @@ import Button from "../../../../components/Button";
 export default class Tag1 extends BaseContainer {
 
     constructor(scene, x, y) {
-        super(scene, x ?? 0, y ?? 0);
+        super(scene, x ?? -0.23177355279381118, y ?? -0.3491356795260714);
 
         /** @type {Phaser.GameObjects.Text} */
         this.name;
@@ -22,29 +22,31 @@ export default class Tag1 extends BaseContainer {
         this.item = 0;
 
 
+        this.alpha = 0.6;
+
         // buy1
-        const buy1 = scene.add.image(0, 0, "clothingcatalog", "buy1");
+        const buy1 = scene.add.image(-34, -26, "clothingcatalog", "buy1");
         buy1.setOrigin(0, 0);
         this.add(buy1);
 
         // hover-cntr
-        const hover_cntr = scene.add.container(-122, 26);
+        const hover_cntr = scene.add.container(0.19456634126533642, 0.3343301292037787);
         hover_cntr.visible = false;
         this.add(hover_cntr);
 
         // rectangle_1
-        const rectangle_1 = scene.add.rectangle(0, 24, 200, 100);
+        const rectangle_1 = scene.add.rectangle(-156.19456635474256, 23.665669875852217, 200, 100);
         rectangle_1.isFilled = true;
         hover_cntr.add(rectangle_1);
 
         // rectangle
-        const rectangle = scene.add.rectangle(0, 24, 210, 110);
+        const rectangle = scene.add.rectangle(-156.19456635474256, 23.665669875852217, 210, 110);
         rectangle.isFilled = true;
         rectangle.fillAlpha = 0.5;
         hover_cntr.add(rectangle);
 
         // name
-        const name = scene.add.text(0, 0, "", {});
+        const name = scene.add.text(-156.19456635474256, -0.3343301241477844, "", {});
         name.setOrigin(0.5, 0.5);
         name.text = "Item Name";
         name.setStyle({ "align": "center", "color": "#000000ff", "fixedWidth":200,"fontFamily": "Burbank Small", "fontSize": "22px", "fontStyle": "bold", "maxLines":2});
@@ -52,7 +54,7 @@ export default class Tag1 extends BaseContainer {
         hover_cntr.add(name);
 
         // price
-        const price = scene.add.text(0, 46, "", {});
+        const price = scene.add.text(-156.19456635474256, 45.66566987585222, "", {});
         price.setOrigin(0.5, 0.5);
         price.text = "8888";
         price.setStyle({ "align": "center", "color": "#000000ff", "fontFamily": "Burbank Small", "fontSize": "30px", "fontStyle": "bold" });
@@ -60,7 +62,7 @@ export default class Tag1 extends BaseContainer {
         hover_cntr.add(price);
 
         // coin
-        const coin = scene.add.image(64, 46, "main", "card-coin");
+        const coin = scene.add.image(-92.19456635474258, 45.66566987585222, "main", "card-coin");
         coin.setOrigin(1, 0.5);
         hover_cntr.add(coin);
 
@@ -77,6 +79,7 @@ export default class Tag1 extends BaseContainer {
 
         /* START-USER-CTR-CODE */
         setTimeout(() => {
+            this.alpha = 1;
             this.hover_cntr.rotation = -this.rotation;
             const item = this.world.getCrumb('items', this.item);
             this.name.text = item.name;
--- src/scenes/interface/catalogs/clothing/buttons/Tag2.js
+++ src/scenes/interface/catalogs/clothing/buttons/Tag2.js
@@ -22,29 +22,31 @@ export default class Tag2 extends BaseContainer {
         this.item = 0;
 
 
+        this.alpha = 0.6;
+
         // buy2
-        const buy2 = scene.add.image(0, 0, "clothingcatalog", "buy2");
+        const buy2 = scene.add.image(-93, -26, "clothingcatalog", "buy2");
         buy2.setOrigin(0, 0);
         this.add(buy2);
 
         // hover-cntr
-        const hover_cntr = scene.add.container(238, 34);
+        const hover_cntr = scene.add.container(-0.014069804748811254, -0.012751264656197803);
         hover_cntr.visible = false;
         this.add(hover_cntr);
 
         // rectangle_1
-        const rectangle_1 = scene.add.rectangle(0, 24, 200, 100);
+        const rectangle_1 = scene.add.rectangle(145.0140697908624, 32.01275126554041, 200, 100);
         rectangle_1.isFilled = true;
         hover_cntr.add(rectangle_1);
 
         // rectangle
-        const rectangle = scene.add.rectangle(0, 24, 210, 110);
+        const rectangle = scene.add.rectangle(145.0140697908624, 32.01275126554041, 210, 110);
         rectangle.isFilled = true;
         rectangle.fillAlpha = 0.5;
         hover_cntr.add(rectangle);
 
         // name
-        const name = scene.add.text(0, 0, "", {});
+        const name = scene.add.text(145.0140697908624, 8.012751265540414, "", {});
         name.setOrigin(0.5, 0.5);
         name.text = "Item Name";
         name.setStyle({ "align": "center", "color": "#000000ff", "fixedWidth":200,"fontFamily": "Burbank Small", "fontSize": "22px", "fontStyle": "bold", "maxLines":2});
@@ -52,7 +54,7 @@ export default class Tag2 extends BaseContainer {
         hover_cntr.add(name);
 
         // price
-        const price = scene.add.text(0, 46, "", {});
+        const price = scene.add.text(145.0140697908624, 54.01275126554041, "", {});
         price.setOrigin(0.5, 0.5);
         price.text = "8888";
         price.setStyle({ "align": "center", "color": "#000000ff", "fontFamily": "Burbank Small", "fontSize": "30px", "fontStyle": "bold" });
@@ -60,7 +62,7 @@ export default class Tag2 extends BaseContainer {
         hover_cntr.add(price);
 
         // coin
-        const coin = scene.add.image(53, 46, "main", "card-coin");
+        const coin = scene.add.image(198.0140697908624, 54.01275126554041, "main", "card-coin");
         coin.setOrigin(1, 0.5);
         hover_cntr.add(coin);
 
@@ -77,6 +79,7 @@ export default class Tag2 extends BaseContainer {
 
         /* START-USER-CTR-CODE */
         setTimeout(() => {
+            this.alpha = 1;
             this.hover_cntr.rotation = -this.rotation;
             const item = this.world.getCrumb('items', this.item);
             this.name.text = item.name;
